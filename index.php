67f9764e758d95da0f9000d95a01f839d80a639d
liujinsong <liujinsong@epptime.com>
2011-10-07 12:03:55 +0800

    修改入口文件

--- index.php
+++ index.php
@@ -1,17 +1,22 @@
 <?php
+	//定义根目录地址
+	define('ONLINE_PATH', '/var/www/yerya/');
 	//定义内核地址
-	define('THINK_PATH','../../webroot/Kernel/ThinkPHP');
-    //定义项目名称
+	define('THINK_PATH',ONLINE_PATH.'webroot/Kernel/ThinkPHP');
+    
+	//定义项目名称
 	define('APP_NAME','App');
     //定义项目路径
-	define('APP_PATH','../../webroot/App');
-    //定义运行碎片路径
-	define('RUNTIME_PATH','../../runtime/');  
+	define('APP_PATH',ONLINE_PATH.'webroot/App');
+    
+	//定义运行碎片路径
+	define('RUNTIME_PATH',ONLINE_PATH.'runtime/');  
     //定义是否产生框架编译
 	define('NO_CACHE_RUNTIME',True);
     //定义框架日志文件路径 
-	define('LOG_PATH','../../runtime/Log/Frame/'); 
-    //运行内核
+	define('LOG_PATH',ONLINE_PATH.'runtime/Log/Frame/'); 
+    
+	//运行内核
 	require(THINK_PATH."/ThinkPHP.php");
     //实例化内核
    	App::run();
