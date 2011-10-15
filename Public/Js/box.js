a1775f44199ad7d31cb0c3eee006d47c290e8a2a
shiyang <shiyang@epptime.com>
2011-10-15 21:11:01 +0800

    更新CSS文件，继续JS框架迁移

--- Public/Js/box.js
+++ Public/Js/box.js
@@ -1,226 +1,59 @@
-/*
- * Based on YUI 3.4.1 (build 4118)
- * Copyright 2011 yerya.com All rights reserved.
- */
-/*
- * bootstrap
- * DOM access code according to different
- */
+$(function () {
+/**
++----------------------------------------------------------------
+* Class    : Box 
+* Property : uri, XHRmethod 
+* Method   : req(option)
++----------------------------------------------------------------
+*/
+    var Box = function (uri, handle, XHRmethod) {
+        // Reg this pointer
+        var _this = this;
 
-YUI().use('node', function (Y) {
+        // Server path 
+        this.uri = uri;
 
-    /*
-     * Sub scroll menu in index page
-     */
+        // XHR method
+        this.XHRmethod = XHRmethod || "GET"; 
 
-    var menu = Y.one("#topBar");
-    if (menu != null) {
-        var hh    = parseInt(Y.one("#header").getStyle("height"));
-        var accul = Y.one("#accul");
-        var accS  = Y.one("#accS");
-        YUI().use('event', 'transition', function (Y) {
-    /*
-     * Fixed the top menu in browser
-     */
-            Y.on('scroll', function (Y) {
-                ( menu.get("docScrollY") <= hh ) ? menu.removeClass("topBarFixed") : menu.addClass("topBarFixed");
+        // Afer XHR finished handle
+        this.handle = handle;
+
+        // Ajax Initial
+        this.req = function (option) {
+
+            // Request to send
+            URL = _this.uri + option;
+
+            // XHR and handle
+            $.ajax({
+                type    : _this.XHRmethod,
+                url     : URL,
+                cache   : false,
+                success : function (data) {
+                    _this.handle(data);
+                }
             });
-
-    /*
-     * Toggle menu : account and message
-     */
-            Y.one("#accS").on('mousemove', function (Y) { accul.show(); accS.addClass("accSDown"); });
-            Y.on('click', function (e) { if ( e.target.get('id') != 'accS' ) { accul.hide(); accS.removeClass("accSDown"); }; });
-        });
+        };
     };
 
-    /*
-     * Handle box 
-     * Extending Base for Box MVC
-     */
-    var boxWrap    = Y.one("#boxWrap");
-    var blankBlock = Y.one("#blankBlock"); 
-    var body       = Y.one('body');
-    var refrB     = Y.one('#refrB a');
+/**
++----------------------------------------------------------------
+* Control all box action
++----------------------------------------------------------------
+*/
+    var boxWrap = $("#boxWrap");
+    if ( boxWrap[0]) {
+        // Initial Render
+        var renderBox = function (data) {
+            boxWrap.data("boxes")= $.parseJSON(data);
+            console.dir(boxWrap.data("boxes"));
+            
+        };
 
-    if ( boxWrap != null ) {
-        YUI().use('model', 'view', 'transition', 'io', function (Y) {
-    /*
-     * Model: Update box
-     * 'uri'   : property,  Storage request link;
-     * 
-     * 'format': method,    Format JSON type data to js object;
-     */
-            Y.UpdateBox = Y.Base.create('UpdateBox', Y.Model, [], {
-    /*
-     * Format JSON to array,bake it by format(i)
-     */
+        // Instantiate Box
+        var updateBox = new Box("../box.php", renderBox);
+        updateBox.req("?page=0");
 
-                format : function (index) {
-                    try{
-                        var res   = Y.JSON.parse(this.data);
-                            index = index || 0;
-                        this.set('imagePath',res[index]);
-                        return res.length;
-                    }
-                    catch(error) {
-                        return 0;
-                    };
-                }
-            }, {
-                ATTRS  : {
-                    imagePath : {
-                        value: '/Public/Images/tempBlock.png'
-                    }
-                }
-            });
-
-    /*
-     * Render box by View's mehod render()
-     */
-
-            Y.RenderBox = Y.Base.create('renderBox', Y.View, [], {
-
-                container   : boxWrap,
-
-                template    : '<div class="block"><img src="{imagePath}" /></div>',
-
-                origin      : 0,
-
-                render      : function (page) {
-                    var page = page,
-                        len  = this.model.format(); 
-                        
-                    this.origin = page*len;
-
-                    if (len != 0) {
-
-                        for (var i=0; i<len; i++) {
-                            this.model.format(i);
-                            this.container.insert(Y.Lang.sub(
-                                this.template, this.model.getAttrs(['imagePath'])
-                            ));
-                        };
-                        blankBlock.setStyle('height', '70px');
-    /*
-     * Animation of boxes,after the render method
-     */
-                        var all  = Y.all('.block');
-                        var size = all.size();
-                        var i   = this.origin-1;
-                        function anim () {
-                            i+=1;
-                            if (i<size) {
-                                all.item(i).transition({
-                                    duration : 0.12,
-                                    easing   : 'ease-in-out',
-                                    opacity  : 1 
-                                    //width    : '160px',
-                                    //height   : '160px'
-                                }, function (){anim()});
-                            }
-                            else {
-                                return false;
-                            }
-                        };
-                        anim();
-                    }
-                    else {
-                        blankBlock.setStyle('height', '90px');
-                        blankBlock.setContent('没有更多了');
-                    };
-                }
-            });
-
-    /*
-     * Handle render and data of boxes
-     * 'req'   : method,    Send XHR with page number, and get response data, put data to this.model.data, go this.render
-     * 'uri'   : property,  Initial the XHR's uri
-     */
-            Y.HandleBox = Y.Base.create('handleBox', Y.Base, [], {
-                view   : '',
-
-                model  : '',
-
-                page   : 0,
-
-                uri    : '../box.php?page=',
-
-                req    : function (page) {
-                    var page = page;
-                    
-                    var uri  = this.uri+page;
-
-                    var that = this;
-
-                    var cfg  = {
-                        method : 'GET',
-                        on     : {
-                            start    : function () {
-                                boxWrap.addClass('loading');
-                                blankBlock.setStyle('height', '90px');
-                            },
-                            complete : function (id, o) {
-                                that.model.data = o.responseText;
-                                that.view.render(page);
-                                boxWrap.removeClass('loading');
-                            },
-                            failure  : function () {
-                                blankBlock.setStyle('height', '90px');
-                                blankBlock.setContent('网络故障，请检查网络链接');
-                            }
-                        }
-                    };
-
-                    Y.io(uri, cfg);
-                },
-
-                handle : function () {
-                    var page = this.page;
-                    var that = this;
-                    this.req(page)
-                    
-                    Y.on('scroll', function () {
-                        var scrlH = boxWrap.get('docScrollY'),
-                            docH  = body.get('docHeight'),
-                            scrnH = body.get('winHeight'),
-                            bool;
-
-                        ( scrlH + scrnH == docH ) ? bool = 1 : bool = 0;
-
-                        if ( bool ==1) {
-                            page+=1;
-                            that.req(page);
-                        }
-                        else {
-                            return false;
-                        };
-                    });
-                },
-
-                init   : function (view, model) {
-                    this.view = view;
-                    this.model= model;
-                } 
-            });
-    /*
-     * Instantiated M V C
-     */
-            var boxM = new Y.UpdateBox();
-            var boxV = new Y.RenderBox({model: boxM});
-            var boxC = new Y.HandleBox();
-                boxC.init(boxV, boxM);
-    /*
-     * Run this Contoller
-     */
-            boxC.handle();
-    /*
-     * Refresh the page
-     */
-            refrB.on('click', function () {
-                boxWrap.setContent('');
-                boxC.req(0);
-            });
-        });
     };
 });
---
+++ Public/Js/main.js
@@ -0,0 +1,72 @@
+$(function () {
+/**
++----------------------------------------------------------------
+* Determine the browser version
++----------------------------------------------------------------
+*/
+    var ie6 = function () { if ( $.browser.msie && $.browser.version.slice(0, 3) == "6.0") return true;}
+
+/**
++----------------------------------------------------------------
+* Control the behavior of all the menu
++----------------------------------------------------------------
+*/
+    var menu        = $("#topBar");
+    var accS        = $("#accS"); 
+    var accul       = $("#accul");
+    if ( menu[0]) {
+        // When scroll,fix the menu to the top of this window
+        if ( !ie6()) {
+            $(window).scroll(function () {
+                ( $(window).scrollTop() <= 90 ) ? menu.removeClass("topBarFixed") : menu.addClass("topBarFixed");
+            });
+        };
+
+        // Account Submenu control
+        accS.click(function () {
+            accS.addClass("accSDown");
+            accul.slideDown('fast');
+        });
+
+        // All cancel click event
+        $(window).click(function (e) {
+            var tar = e.target.id;
+
+            // Delegate all cancel click event
+            if ( tar != "accS" ) {
+                accS.removeClass("accSDown");
+                accul.slideUp('fast');
+            };
+        });
+    };
+
+/**
++----------------------------------------------------------------
+* Control the behavior of block'tool
++----------------------------------------------------------------
+*/
+    var boxTool     = $("#boxTool");
+    var makeB       = $("#makeB a");
+    var makeBul     = $("#makeBul");
+    var makeBarr    = $("#makeBarr");
+    var makeBcancel = $("#makeB .cancel")
+    if ( boxTool[0]) {
+        // Customize block Submenu control
+        makeB.click(function () {
+            makeB.addClass("makeBDown");
+            makeBul.fadeIn(200);
+            makeBarr.show();
+        });
+
+        // Cancel Customize block dialog
+        makeBcancel.click(function () {
+            makeB.removeClass("makeBDown");
+            makeBul.hide();
+            makeBarr.hide();
+        });
+    };
+$.getScript("../Js/box.js", function() {
+    alert("get box.js")
+});
+});
+
