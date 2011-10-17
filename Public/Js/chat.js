805ebdbf1616b97ad88232716d81dde34608756d
songjieqing <songjieqing@epptime.com>
2011-10-17 20:16:14 +0800

    增加聊天页，相关CSS，html，js

---
+++ Public/Js/chat.js
@@ -0,0 +1,41 @@
+/**
++----------------------------------------------------------------
+* handle Chat Choose 
++----------------------------------------------------------------
+*/
+$(function(){
+
+   //Handle every Items(Person) hover behavior
+   $('.S_item').each(function(){
+      $(this).hover(function(){
+          $(this).css('backgroundColor','#E0E0E0');
+          $(this).find('.S_button').show();
+      },function(){
+          $(this).css('backgroundColor','');
+          $(this).find('.S_button').hide();
+      })
+   });
+   
+   //Handle  BottomBar's items for detail
+   $('.S_bottomBar .items').each(function(i){
+      $(this).click(function(){
+		  $('.S_bottomBar .items').css('background',"url('../Images/S_bottomBar.png') repeat-x");			 
+          $(this).css('background',"url('../Images/bmBarhover.png') repeat-x");
+          $('#S_top').slideUp('slow');
+		  $('.S_line').fadeOut('normal');
+		  $(".S_layer").show();
+          $('#S_all').show();//the layer Show 
+		  $('.S_layer').find('.S_list').removeClass("grey").eq(i).addClass("grey")
+      })
+   })
+   
+   //Handle mouseout Layer hide behavier
+   
+   //handle the layerItems 's hover behaivor
+   $(".S_list").each(function(i){
+		$(this).hover(function(){
+			$(".items").eq(i).click();
+		})						  
+   })
+})
+
