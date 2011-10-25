dc9c674209860ddb507d8546e8e11fefe2060aa2
yangqingyu <yangqingyu@epptime.com>
2011-10-25 16:47:42 +0800

    更新上传日志和上传图像的js功能

--- Public/Js/box.add.js
+++ Public/Js/box.add.js
@@ -107,6 +107,8 @@ $(function(){
 		this.sendReq = function ( option ) {
 			//solve IE cache question
 			var second = new Date();
+			
+			var option = option || "?";
 		
 			var thisUrl = _this.uri + option + "&null=" + second.getTime();
 			 
@@ -126,6 +128,7 @@ $(function(){
 $(function() {
 	
 	var clickButton = $('#Y_sendArticle input');
+	
 	clickButton.click(function() {
 		
 		//get the title
@@ -142,10 +145,16 @@ $(function() {
 			var tagValue = $(this).text();
 			getTags.push( tagValue );	
 		})
-		var getTagString = getTags.toString();
+		
+		
+		if ( getTags == "" ){
+			var getTagString = getTags.toString( getTags );
+		}else{
+			var getTagString = JSON.stringify( getTags );
+		}
 		
 		var postJsondata = "&title=" + getTitle + "&type=0" +"&content=" + getHTML +"&tag=" + getTagString;
-		
+
 		//send ajax request
 		
 		var postUri = "/grid/add/";
@@ -166,7 +175,9 @@ $(function() {
 					window.location.href = '/';
 				})
 			}else{
-				clickButton.val("发布错误哦，亲");
+				if ( answerInfo == '请先登录'){
+					clickButton.val( '啊哦，还没登录哦，亲~');
+				}
 			}
 			}
 		
@@ -214,11 +225,7 @@ $(function(){
 	})
 	
 	imgInput.focus(function() {
-		imgInput.val("");
-	})
-	
-	imgInput.blur(function() {
-		imgInput.val("为本组图片添加标题");
+		imgInput.val("").css('color','#474747');
 	})
 	
 	//addText button click event
@@ -231,9 +238,12 @@ $(function(){
 	
 		// Create a new editor inside the <div id="editor">, setting its value to html
 		var config = {
-			height:'76px;'
+			height:'76px;',
+			name:'eidtor2'
 			};
 		editor = CKEDITOR.appendTo( 'Y_imgTextarea', config, html);
+		
+		
  
 	}
 
@@ -316,6 +326,85 @@ $(function(){
 	//box.add.image.html fabu button's click event
 	
 	var imgSendButton = $('#Y_sendImg input');
+	
+	imgSendButton.click(function(){
+		//get picture's title
+		
+		var imgTitleVal = $('.Y_imgInput input').val();
+		
+		//get picture's content value
+		
+		var imgGetHtml = editor.document.getBody().getText().toString();
+		
+		//get picture's tag value
+		
+		var imgTagVal = [];
+		$('#Y_tagUl li span').each(function() {
+			var imgTagText = $(this).text();
+			imgTagVal.push( imgTagText );
+		})
+		
+		var imgTagString;
+		if ( imgTagVal == ""){
+			imgTagString = imgTagVal.toString();
+		}else{
+			imgTagString = JSON.stringify( imgTagVal );
+		}
+		
+		//get picture's thumb url
+		
+		var imgThumbVal = [];
+		$('#Y_imgShow li img').each(function() {
+			var thisSrc = $(this).attr("src");
+			imgThumbVal.push( thisSrc );
+		})
+		
+		var imgThumbString;
+		if( imgTagVal == ""){
+			imgThumbString = imgThumbVal.toString();
+		}else{
+			imgThumbString = JSON.stringify( imgThumbVal );
+		}
+
+		//post url
+		
+		var imgPostUri = "/grid/add/";
+		
+		//post data
+		
+		var imgPostData = "&title=" + imgTitleVal + "&type=1" + "&thumb=" + imgThumbString + "&content=" + imgGetHtml + "&tag=" + imgTagString;
+		
+		//callback function after send request
+		
+		var imgCallFun = function( data ){
+			var backObj = $.parseJSON( data );
+			
+			var imgStatus = backObj.status;
+			var imgInfo = backObj.info;
+			var imgData = backObj.data;
+			
+			if ( imgStatus == '0'){
+				imgSendButton.val("亲，出现问题了")
+			}else{
+				imgSendButton.val("亲，上传成功了 ，鼓掌~")
+			}
+			 
+		}
+		
+		//create new ajax request
+		var imgPostMethod = "POST";
+		
+		if ( imgGetHtml == ""){
+			alert('请填写内容再发布哦，亲')
+		}else if ( imgTitleVal == ""){
+			alert('还未填写标题哦，亲')
+		}else{
+			var imgObj = new boxPost( imgPostUri, imgCallFun, imgPostMethod, imgPostData);
+			imgObj.sendReq();
+			imgSendButton.val("发布中");
+		}
+		
+	})
 })
 
 
