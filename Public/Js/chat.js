2a170e23eb6f2b70407d9a8c1bd2c98903f29847
songjieqing <songjieqing@epptime.com>
2011-10-27 17:26:25 +0800

    分离聊天js，添加chat_ing.js

--- Public/Js/chat.js
+++ Public/Js/chat.js
@@ -1,4 +1,4 @@
-/*
+/**
 +----------------------------------------------------------------
 * handle Chat Choose 
 +----------------------------------------------------------------
@@ -96,48 +96,6 @@ $(function(){
 	 })  
   });
   
-  //handle submit talk contents
-  
-  $(".S_font1").click(function(){
-  	  if($('#S_div_box').html()!=''){
-	  	 //ajax submit content
-		 $.ajax({
-			url:'/chat/ajaxPushMessage',
-			data:'content='+$('#S_div_box').html(),
-			type:'post',
-			success:function(msg){
-		       var content= $('.S_t ul').find('.S_mc').eq(0).clone(true);
-               var obj=eval("("+msg+")");
-               content.find('.S_tcBox').children('.S_Smid').html(obj.data.text);
-               content.appendTo($(".S_t ul"));
-               content.focus();
-            },
-            error:function(){
-                $('.S_font1').click();
-            }
-		 });
-	  }
-  })
-  if($("#record_content")[0]){
-   setInterval(function(){
-     $.ajax({
-			url:'/chat/ajaxPullMessage',
-			data:'content=1',
-			type:'post',
-			success:function(msg){
-		       var content= $('.S_t ul').find('.S_tc').eq(0).clone(true);
-               var obj=eval("("+msg+")");
-               content.find('.S_tcBox').children('.S_Smid').html(obj.data.text);
-               content.appendTo($(".S_t ul"));
-               content.focus();
-            },
-            error:function(){
-                $('.S_font1').click();
-            }
-		 });
-    },1000); 
-  } 
-     	 
   //handle top three items on chatPage
 	  //handle click
 	  function check(obj){
@@ -175,9 +133,9 @@ $(function(){
       //handle mouseover  on Top Three Items
 	  $('.S_bannerList li').each(function(i){
   	  	 $(this).hover(function(){
-	  	 	$(this).removeClass('items_click').addClass('items_hover');
+	  	 	$(this).css({"background":"url(../Images/barhover.png) no-repeat"});
 	  	 },function(){
-			 $(this).removeClass('items_hover').addClass('items_click');
+			 $(this).css({"background":"url(../Images/barclick.png) no-repeat"});
 		 })
   	  })
    
---
+++ Public/Js/chat_ing.js
@@ -0,0 +1,38 @@
+$(function(){
+  if($("#record_content")[0]){
+  $(".S_font1").click(function(){
+  	  if($('#S_div_box').html()!=''){
+	  	 //ajax submit content
+		 $.ajax({
+			url:'/chat/chatpage',
+			data:$('#S_div_box').html(),
+			type:'post',
+			success:function(msg){
+		       var content= $('.S_t ul').find('.S_tc').clone(true);
+               content.find('S_tcBox').children('.S_Smid1').html(msg);
+            },
+            error:function(){
+                $('.S_font1').click();
+            }
+		 });
+	  }
+  });
+   setInterval(function(){
+     $.ajax({
+			url:'/chat/ajaxPullMessage',
+			data:'content=1',
+			type:'post',
+			success:function(msg){
+		       var content= $('.S_t ul').find('.S_tc').eq(0).clone(true);
+               var obj=eval("("+msg+")");
+               content.find('.S_tcBox').children('.S_Smid').html(obj.data.text);
+               content.appendTo($(".S_t ul"));
+               content.focus();
+            },
+            error:function(){
+                $('.S_font1').click();
+            }
+		 });
+    },1000); 
+  } 
+})
